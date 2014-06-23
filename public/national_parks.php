47183de43d091d9c0f730b9accecb7aa5184d6fa
vagrant <vagrant@codeup-trusty>
2014-06-23 20:29:41 +0000

    modified page to load only 4 parksat a time with links to previous and next pages. disabled links for previous when at the first page and disabled links for next when at the last page

--- public/national_parks.php
+++ public/national_parks.php
@@ -1,13 +1,29 @@
 <?php
-// Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=codeup_pdo_test_db', 'alicia', 'password');
+function getOffset(){
+	$page = isset($_GET['page']) ? $_GET['page'] : 1;
+	return ($page - 1) * 4;
+}
 
-// Tell PDO to throw exceptions on error
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// if (!empty($_GET)) {
+// 	var_dump($_GET);
+// 	$pageID = // get value of page ID here
+// 	$offset = pageID * 4
+// }
 
-$stmt = $dbc->query('SELECT * FROM national_parks');
+//$query = "SELECT * FROM national_parks LIMIT 4 OFFSET $offset;";
 
-$array = ($stmt->fetchAll(PDO::FETCH_ASSOC));
+
+
+$query ='SELECT * FROM national_parks LIMIT 4 OFFSET ' . getOffset();
+$parks = $dbc->query($query)->fetchAll(PDO::FETCH_ASSOC);
+
+//$parks = $dbc->query("SELECT * FROM national_parks LIMIT 4 OFFSET" . getOffset(); )->fetchAll(PDO::FETCH_ASSOC);
+$count = $dbc->query('SELECT count(*) FROM national_parks')->fetchColumn();
+$numPages = ceil($count/4);
+//$array = ($stmt->fetchAll(PDO::FETCH_ASSOC));
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$nextPage = $page + 1;
+$prevPage = $page - 1;
 
 ?>
 
@@ -26,14 +42,21 @@ $array = ($stmt->fetchAll(PDO::FETCH_ASSOC));
 		<th>Area in Acres</th>
 	</tr>
 	<tr>
-<? foreach ($array as $key => $value) :?>
-	<td><?= $value['name'] ?></td>
-	<td><?= $value['location'] ?></td>
-	<td><?= $value['date_established'] ?></td>
-	<td><?= $value['area_in_acres'] ?></td>
+<? foreach ($parks as $park) :?>
+	<td><?= $park['name'] ?></td>
+	<td><?= $park['location'] ?></td>
+	<td><?= $park['date_established'] ?></td>
+	<td><?= $park['area_in_acres'] ?></td>
 	</tr>
 <? endforeach;?>
 </table>
+
+<? if($page > 1):?>
+<button id="back"> <a href="?page=<?= $prevPage; ?>"</a>Previous</button>
+<? endif; ?>
+<? if($nextPage <= $numPages) : ?>
+<button id="next"> <a href="?page=<?= $nextPage; ?>"</a>Next</button>
+<? endif; ?>
 
 </body>
 </html>
